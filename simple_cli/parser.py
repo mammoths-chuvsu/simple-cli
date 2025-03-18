b6cae9504c2ed95e48fd1bc900848637e0570e20
vdaleke <vdalekesmirnov@gmail.com>
2025-03-18 22:19:47 +0300

    feat: add parser class

---
+++ simple_cli/parser.py
@@ -0,0 +1,50 @@
+"""Parser class implementation."""
+
+import shlex
+from typing import List
+
+from pydantic import BaseModel
+
+
+class ParsedCommand(BaseModel):
+    """Represents a parsed command with its name and a list of arguments."""
+
+    name: str
+    args: List[str]
+
+
+class ParsedCommands(BaseModel):
+    """Represents a sequence of parsed commands."""
+
+    command_seq: List[ParsedCommand]
+
+
+class Parser(BaseModel):
+    """Parser class to tokenize and parse command line strings."""
+
+    def _tokenize(self, command: str) -> ParsedCommand:
+        """Tokenize a command string into a ParsedCommand object.
+
+        Args:
+            command (str): The command line string to be tokenized.
+
+        Returns:
+            ParsedCommand: An object containing the command name and arguments.
+        """
+        # Split the command string while considering quotes
+        args: List[str] = shlex.split(command)
+        # Extract the command name from the arguments list
+        name: str = args.pop(0)
+        return ParsedCommand(name=name, args=args)
+
+    def parse(self, line: str) -> ParsedCommands:
+        """Parse a line of command string into ParsedCommands.
+
+        Args:
+            line (str): The command line string to parse.
+
+        Returns:
+            ParsedCommands: An object containing a sequence of parsed commands.
+        """
+        command: ParsedCommand = self._tokenize(line)
+        return ParsedCommands(command_seq=[command])
