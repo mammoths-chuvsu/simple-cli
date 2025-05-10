4c5b1c07a798e042dfd0496b7ee815cfe9ef6d94
Anton Poiskov <74915373+FirePancake1177@users.noreply.github.com>
2025-05-11 01:41:31 +0300

    refactor: Add comments cli-2 (#26)
    * refactor: Add comments cli-2
    
    * fix

--- simple_cli/executor.py
+++ simple_cli/executor.py
@@ -37,15 +37,21 @@ class Executor:
         for idx, command in enumerate(parsed_commands.command_seq):
             command_executor = self._command_storage.get_command(command.name)
 
+            # Create a pipe if there is a next command (i.e., setting up a pipeline)
             stdin_pipe, stdout_pipe = (
                 os.pipe() if idx < num_commands - 1 else (None, None)
             )
+
+            # Use previous stdout as stdin if available (from previous pipe output)
             stdin = os.fdopen(prev_stdin, "r") if prev_stdin is not None else sys.stdin
+
+            # If this is not the last command, set up its stdout to a pipe
             stdout = (
                 os.fdopen(stdout_pipe, "w") if stdout_pipe is not None else sys.stdout
             )
-
             result_code = command_executor.execute(command, stdin, stdout)
 
+            # Save current input pipe to be used as input for next command
             prev_stdin = stdin_pipe
+
         return result_code
